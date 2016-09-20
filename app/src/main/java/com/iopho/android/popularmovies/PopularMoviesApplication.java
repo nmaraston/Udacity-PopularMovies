8249569b3e46a7976201d55d16a12395f429d1a8
nmaraston <nick.maraston@gmail.com>
2016-09-19 23:46:27 -0400

    Read the TMDB API Key from a .properties app config file

--- app/src/main/java/com/iopho/android/popularmovies/PopularMoviesApplication.java
+++ app/src/main/java/com/iopho/android/popularmovies/PopularMoviesApplication.java
@@ -1,9 +1,14 @@
 package com.iopho.android.popularmovies;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.google.common.base.Preconditions;
 import com.iopho.android.dataAccess.tmdb.TMDBClientFactory;
+import com.iopho.android.util.ApplicationProperties;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 /**
  * The PopularMoviesApplication class maintains global application state for the PopularMovies app.
@@ -12,15 +17,28 @@ public class PopularMoviesApplication extends Application {
 
     private static final boolean DEBUG_MODE = true;
 
-    private static final String API_KEY = "b70e10dcfb049ab5616c62edb2946e9e";
-
+    private PopularMoviesAppProperties mApplicationProperties;
     private TMDBClientFactory mTMDBClientFactory;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mTMDBClientFactory = new TMDBClientFactory(API_KEY);
 
+        try {
+            mApplicationProperties = new PopularMoviesAppProperties(this, R.raw.config);
+        } catch (IOException ex) {
+            // TODO: What is a better way of handling an unrecoverable event during application initialization?
+            throw new RuntimeException(
+                    "Failed to construct application properties from resource ID " + R.raw.config,
+                    ex);
+        }
+
+        final String tmdbAPIKey = mApplicationProperties.getStringPropertyValue(
+                PopularMoviesAppProperties.PropertyKey.TMDB_API_KEY);
+
+        mTMDBClientFactory = new TMDBClientFactory(tmdbAPIKey);
+
+        // TODO: What is a better way of managing application environments?
         if (DEBUG_MODE) {
             Picasso.with(this).setIndicatorsEnabled(true);
         }
@@ -31,5 +49,30 @@ public class PopularMoviesApplication extends Application {
      */
     public TMDBClientFactory getTMDBClientFactory() {
         return mTMDBClientFactory;
+    }
+
+    /**
+     * An extension of {@link ApplicationProperties} that enforces (via static typing) that only
+     * keys of type {@link PropertyKey} can be read.
+     */
+    private static class PopularMoviesAppProperties extends ApplicationProperties {
+
+        /**
+         * All possible property key names for the Popular Movies app.
+         */
+        public enum PropertyKey {
+            TMDB_API_KEY;
+        }
+
+        public PopularMoviesAppProperties(final Context context, final int configFileId)
+                throws IOException {
+            super(context, configFileId);
+        }
+
+        public String getStringPropertyValue(final PropertyKey key) {
+
+            Preconditions.checkNotNull(key, "key must not be null.");
+            return super.getStringPropertyValue(key.name());
+        }
     }
 }
---
+++ app/src/main/java/com/iopho/android/util/ApplicationProperties.java
@@ -0,0 +1,63 @@
+package com.iopho.android.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import com.google.common.base.Preconditions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/*
+ * ApplicationProperties provides a general interface for reading key-values from a .properties
+ * resource file.
+ */
+public class ApplicationProperties {
+
+    private static final String LOG_TAG = ApplicationProperties.class.getSimpleName();
+
+    private final Properties mProperties;
+
+    /**
+     * Construct a new ApplicationProperties instance.
+     *
+     * @param context the {@link Context} containing the
+     * @param configFileId resource ID for the .properties resource file to read in key-value
+     *                     properties from.
+     * @throws IOException if unable to open/read the .properties file indicated by
+     * <b>configFileId</b>
+     */
+    public ApplicationProperties(final Context context, final int configFileId) throws IOException {
+
+        Preconditions.checkNotNull(context, "context must not be null.");
+
+        final Resources resources = context.getResources();
+
+        try {
+            InputStream resourceIn = resources.openRawResource(configFileId);
+            mProperties = new Properties();
+            mProperties.load(resourceIn);
+        } catch (Resources.NotFoundException ex) {
+            Log.e(LOG_TAG, "Unable to find resource for ID " + configFileId);
+            throw ex;
+        } catch (IOException ex) {
+            Log.e(LOG_TAG, "Failed to read resource file for ID " + configFileId);
+            throw ex;
+        }
+    }
+
+    /**
+     * The value of the property associated with the given key. Returns null if the property key is
+     * not found.
+     *
+     * @param key the property key
+     * @return the value associated with the given property key or null if non exists.
+     */
+    public String getStringPropertyValue(final String key) {
+
+        Preconditions.checkNotNull(key, "key must not be null.");
+        return mProperties.getProperty(key);
+    }
+}
