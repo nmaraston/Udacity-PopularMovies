02df77cb16c68f899370e5e21deef08045a593a9
nmaraston <nick.maraston@gmail.com>
2016-09-24 22:01:53 -0400

    Remove floating action button

--- app/src/main/java/com/iopho/android/popularmovies/MovieGalleryActivity.java
+++ app/src/main/java/com/iopho/android/popularmovies/MovieGalleryActivity.java
@@ -2,17 +2,12 @@ package com.iopho.android.popularmovies;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MovieGalleryActivity extends AppCompatActivity {
-
-    private static final String LOG_TAG = MovieGalleryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +15,6 @@ public class MovieGalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_gallery);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
